1ea32e5f8a584a312052530353ae3bc77b2631f8
Esteban Le Calvez <lecalvez.esteban@gmail.com>
2019-11-12 15:16:58 +0100

    Ajout modif et suppression du tip.

--- front/src/components/Tip.js
+++ front/src/components/Tip.js
@@ -1,25 +1,71 @@
 import React from "react";
 import { withStyles } from "@material-ui/styles";
 import firebase from "firebase";
-import { Typography, Container, CircularProgress } from "@material-ui/core";
+import DeleteIcon from '@material-ui/icons/Delete';
+import CreateIcon from '@material-ui/icons/Create';
+import { Typography, Container, CircularProgress, Fab, Dialog, DialogContent, DialogTitle, Button, DialogActions, TextField } from "@material-ui/core";
+import FullscreenIcon from '@material-ui/icons/Fullscreen';
+import FullscreenExitIcon from '@material-ui/icons/FullscreenExit';
+import ReactQuill from "react-quill";
 
 const styles = theme => ({
   pageContent: {
     margin: 100
+  },
+  floatingActionButton: {
+    bottom: 20,
+    right: 20,
+    position: "fixed",
+    color: "white",
+    backgroundColor: "red",
+    "&:hover": {
+      backgroundColor: "darkRed",
+      color: "lightGrey"
+    }
+  },
+  floatingActionButtonTop: {
+    top: 80,
+    right: 20,
+    position: "fixed",
+    color: "white",
+    backgroundColor: "green",
+    "&:hover": {
+      backgroundColor: "darkGreen",
+      color: "lightGrey"
+    }
   },
 });
 
 class Tip extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { tip: {}, charging: true };
+    this.state = { tip: {}, isDialogFullScreen: false, charging: true, openModification: false, openDeletion: false, newname: "", newdesc: "", newtext: "" };
     this.fetchTip = this.fetchTip.bind(this);
+    this.deleteTip = this.deleteTip.bind(this);
+    this.modifyTip = this.modifyTip.bind(this);
+    this.handleClickOpenModification = this.handleClickOpenModification.bind(this);
+    this.handleClickCloseModification = this.handleClickCloseModification.bind(this);
+  }
+  handleClickOpenDeletion = () => {
+    this.setState({ openDeletion: true });
+  };
+
+  handleClickCloseDeletion = () => {
+    this.setState({ openDeletion: false });
+  };
+
+  handleClickOpenModification() {
+    this.setState({ openModification: true });
   }
 
+  handleClickCloseModification() {
+    this.setState({ openModification: false });
+  }
 
   fetchTip() {
     const db = firebase.firestore();
     var currentTip = {}
+    this.setState({ charging: true });
     var docRef = db.collection("tips").doc(this.props.match.params.id);
     docRef.get().then(doc => {
       if (doc.exists) {
@@ -27,6 +73,9 @@ class Tip extends React.Component {
         console.log("Document data:", doc.data());
         this.setState({
           tip: currentTip,
+          newtext: currentTip.text,
+          newdesc: currentTip.description,
+          newname: currentTip.name,
           charging: false
         })
       } else {
@@ -40,14 +89,77 @@ class Tip extends React.Component {
 
   }
   componentDidMount() {
+
     this.fetchTip();
     console.log("state after fetchTip in cdm", this.state);
   }
 
+  deleteTip() {
+    const db = firebase.firestore();
+    db.collection("tips").doc(this.props.match.params.id).delete().then(function () {
+      this.handleClickCloseDeletion();
+    }).catch(function (error) {
+      console.error("Error removing document: ", error);
+    });
+    this.props.history.push("/folders/" + this.state.tip.gameId);
+  }
+
+
+  handleClickFullscreen = () => {
+    this.setState({ isDialogFullScreen: true });
+
+  };
+
+  handleClickNotFullscreen = () => {
+    this.setState({ isDialogFullScreen: false });
+  };
+
+
+  modifyTip() {
+    const db = firebase.firestore();
+    db.collection("tips").doc(this.props.match.params.id).update({
+      text: this.state.newtext,
+      description: this.state.newdesc,
+      name: this.state.newname,
+      lastModifiedOn: new Date()
+    }).then(() => {
+      this.handleClickCloseModification();
+      this.fetchTip();
+    });
+  }
+
+  changeName = event => {
+    this.setState({
+      newname: event.target.value
+    });
+  };
+
+  changeDescription = event => {
+    this.setState({
+      newdesc: event.target.value
+    });
+  };
+
   render() {
     const { classes } = this.props;
 
-    const { tip, charging } = this.state;
+    const { tip, charging, isDialogFullScreen, newname, newtext, newdesc } = this.state;
+    const modules = {
+      toolbar: [
+        [{ 'header': [1, 2, false] }],
+        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+        [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }],
+        ['link', 'image'],
+        ['clean']
+      ],
+    };
+
+    const formats = [
+      'header',
+      'bold', 'italic', 'underline', 'strike', 'blockquote',
+      'list', 'bullet', 'indent',
+      'link', 'image'
+    ];
     console.log("state in render", this.state);
     return (
       <React.Fragment>
@@ -58,10 +170,94 @@ class Tip extends React.Component {
                 {tip.name}
               </h1>
               <div dangerouslySetInnerHTML={{ __html: "<Typography>" + tip.text + "</Typography>" }}></div>
+              <Fab aria-label="delete" className={classes.floatingActionButton}>
+                <DeleteIcon onClick={this.handleClickOpenDeletion} />
+              </Fab>
+              <Fab aria-label="modification" className={classes.floatingActionButtonTop}>
+                <CreateIcon onClick={this.handleClickOpenModification} />
+              </Fab>
             </React.Fragment>
           )
           }
 
+          <Dialog
+            open={this.state.openDeletion}
+            onClose={this.handleClickCloseDeletion}
+          > <DialogTitle id="alert-dialog-slide-title">
+              {"Etes vous sûr?"}
+            </DialogTitle>
+            <DialogContent>
+              Cette action est irréversible
+              </DialogContent>
+            <DialogActions>
+              <Button onClick={this.deleteTip} color="primary">
+                Supprimer
+            </Button>
+              <Button onClick={this.handleClickCloseDeletion} color="primary">
+                Annuler
+            </Button>
+            </DialogActions>
+          </Dialog>
+
+          <Dialog
+            open={this.state.openModification}
+            fullWidth
+            fullScreen={isDialogFullScreen}
+            onClose={this.handleClickCloseModification}
+            className={classes.dialog}
+          >
+            <DialogTitle id="alert-dialog-slide-title">
+              {"Créer une astuce"}
+              {isDialogFullScreen ?
+                <Button onClick={this.handleClickNotFullscreen}><FullscreenExitIcon /></Button> :
+                <Button onClick={this.handleClickFullscreen}><FullscreenIcon /></Button>
+              }
+            </DialogTitle>
+
+            <DialogContent>
+              <form noValidate autoComplete="off">
+                <TextField
+                  id="tip-name"
+                  className={classes.textField}
+                  label="Nom de l'astuce"
+                  value={newname}
+                  onChange={this.changeName}
+                />
+                <p></p>
+                <TextField
+                  id="tip-description"
+                  className={classes.textField}
+                  label="Description de l'astuce"
+                  value={newdesc}
+                  onChange={this.changeDescription}
+                />
+                <p></p>
+                <p>Contenu de l'astuce</p>
+                <ReactQuill
+                  theme="snow"
+                  modules={modules}
+                  formats={formats}
+                  value={newtext}
+                  onChange={(value) => {
+                    this.setState({ newtext: value });
+                    console.log(value);
+                  }}>
+                </ReactQuill>
+                <p></p>
+              </form>
+            </DialogContent>
+
+            {charging ? null :
+              <DialogActions>
+                <Button onClick={this.modifyTip} color="primary">
+                  Modifier
+            </Button>
+                <Button onClick={this.handleClickCloseModification} color="primary">
+                  Annuler
+            </Button>
+              </DialogActions>
+            }
+          </Dialog>
         </div>
       </React.Fragment>
     );
